6a137b8dfd522b75b6ed3258703f78df8716f73f
Xin <letter113@hotmail.com>
2016-06-20 23:18:11 +0200

    * Added unit tests for compile

---
+++ android_build_system/tests/helpers/mock-project-dir/src/com/p/some.java
@@ -0,0 +1,15 @@
+package com.p;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Some extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
